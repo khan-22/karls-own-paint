5f701ff5c8ff940059e10c7f21ff1d5d62efd556
Karl Hansson <khansson.nr2@gmail.com>
2017-09-26 19:18:06 +0200

    test

---
+++ src/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef H_H
+#define H_H
+
+#include <iostream>
+
+void p() {
+	std::cout << "pa" << std::endl;
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Header.h"
+
+#include "imgui.h"
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	p();
+	std::cin.get();
+}
